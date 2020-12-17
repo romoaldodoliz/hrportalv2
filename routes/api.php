e75c9d52cdc9b02ef0213b9356d8b720a3ac13c0
Arjay Lumagdong <arjay.lumagdong@lfuggoc.net>
2020-12-17 17:56:13 +0800

    Login API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Employee;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,32 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/user-login',function(Request $request){
+    $user = User::where('email', $request->email)->first();
+    if ($user) {
+        if (Hash::check($request->password, $user->password)) {
+            return Employee::select('id','user_id','last_name','first_name')->whereHas('user',function($q) use($request){
+                        $q->where('email',$request->email);
+                    })
+                    ->with(array(
+                        'companies',
+                        'departments',
+                        'locations',
+                        'user'=>function($query){
+                            $query->select('id','email');
+                        })
+                    )
+                    ->first();
+        } else {
+            $response = ["message" => "Password mismatch"];
+            return response($response, 422);
+        }
+    } else {
+        $response = ["message" =>'User does not exist'];
+        return response($response, 422);
+    }
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
